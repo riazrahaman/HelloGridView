c853a6c7ab40f7bc26214fa08734838b10ce29f7
Riaz Ur Rahaman <rahamanriaz@gmail.com>
2013-12-25 01:20:09 +0530

    Base version. Has compilation bug that needs to be fixed.

---
+++ gridview/src/main/java/com/riaz/gridview/ImageAdapter.java
@@ -0,0 +1,58 @@
+package com.riaz.gridview;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by riazrahaman on 24/12/13.
+ */
+public class ImageAdapter extends BaseAdapter{
+    private Context mContext;
+
+    private Integer[] mThumbIds = {
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_2, R.drawable.sample_3,
+    };
+
+    public ImageAdapter(Context C){
+        mContext = C;
+    }
+
+    public int getCount(){
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position){
+        return null;
+    }
+
+    public long getItemId(int position){
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        ImageView imageView;
+        if(convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85,85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8,8,8,8);
+        }
+        else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(position);
+        return imageView;
+    }
+
+}
---
+++ gridview/src/main/java/com/riaz/gridview/MainActivity.java
@@ -0,0 +1,140 @@
+package com.riaz.gridview;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.apache.http.HeaderElement;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        GridView mGridView;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onViewCreated(View view, Bundle SavedInstanceState) {
+            mGridView = (GridView) view.findViewById(R.layout.fragment_main);
+            mGridView.setAdapter(new ImageAdapter(this));
+
+            mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                    Toast.makeText(Context context(), "Image Clicked", 0).show();
+                    Log.e("RIAZ", "image clicked");
+                }
+            });
+
+
+            }
+
+        }
+
+    public static class ImageAdapter extends BaseAdapter{
+        private Context mContext;
+
+        private Integer[] mThumbIds = {
+                R.drawable.sample_0, R.drawable.sample_1,
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_0, R.drawable.sample_1,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7,
+                R.drawable.sample_2, R.drawable.sample_3,
+        };
+
+        public ImageAdapter(Context C){
+            mContext = C;
+        }
+
+        public int getCount(){
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position){
+            return null;
+        }
+
+        public long getItemId(int position){
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent){
+            ImageView imageView;
+            if(convertView == null) {
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(85,85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8,8,8,8);
+            }
+            else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(position);
+            return imageView;
+        }
+
+    }
+
+    }
