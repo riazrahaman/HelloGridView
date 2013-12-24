46cd73e8ecf923d64166d6c0fe2aa3055294ccdc
Riaz Ur Rahaman <rahamanriaz@gmail.com>
2013-12-25 02:02:19 +0530

    Moved the adapter to a new class.
    The getActivity is null which is being passed to the constructor of adapter. This is causing the app to crash.

--- gridview/src/main/java/com/riaz/gridview/MainActivity.java
+++ gridview/src/main/java/com/riaz/gridview/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends Activity {
         @Override
         public void onViewCreated(View view, Bundle SavedInstanceState) {
             mGridView = (GridView) view.findViewById(R.layout.fragment_main);
-            mGridView.setAdapter(new ImageAdapter(this));
+            mGridView.setAdapter(new ImageAdapter(getActivity()));
 
             mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
@@ -89,52 +89,4 @@ public class MainActivity extends Activity {
             }
 
         }
-
-    public static class ImageAdapter extends BaseAdapter{
-        private Context mContext;
-
-        private Integer[] mThumbIds = {
-                R.drawable.sample_0, R.drawable.sample_1,
-                R.drawable.sample_2, R.drawable.sample_3,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_0, R.drawable.sample_1,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_6, R.drawable.sample_7,
-                R.drawable.sample_2, R.drawable.sample_3,
-        };
-
-        public ImageAdapter(Context C){
-            mContext = C;
-        }
-
-        public int getCount(){
-            return mThumbIds.length;
-        }
-
-        public Object getItem(int position){
-            return null;
-        }
-
-        public long getItemId(int position){
-            return 0;
-        }
-
-        public View getView(int position, View convertView, ViewGroup parent){
-            ImageView imageView;
-            if(convertView == null) {
-                imageView = new ImageView(mContext);
-                imageView.setLayoutParams(new GridView.LayoutParams(85,85));
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                imageView.setPadding(8,8,8,8);
-            }
-            else {
-                imageView = (ImageView) convertView;
-            }
-
-            imageView.setImageResource(position);
-            return imageView;
-        }
-
-    }
-
-    }
+}
